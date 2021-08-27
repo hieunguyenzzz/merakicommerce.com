7ffae6f9fd6795bdf996c877870ad24a4881ee98
long <ngohoanglongptit@gmail.com>
2021-08-27 10:07:35 +0700

    update contact

--- components/templates/Contact.js
+++ components/templates/Contact.js
@@ -15,7 +15,7 @@ function Contact() {
       <section >
         <Hero {...{
           banner: get('current.banner', get('global.banner')),
-          title: get('current.hero_title')
+          title: 'Contact'
         }} >
           <p style={{ animationDelay: '500ms' }} className="custom-breadcrumbs aos-init aos-animate text-lg animate__animated animate__fadeInUp " data-aos="fade-up" data-aos-delay="100">
             <Link href="/" className="underline">Home</Link> <span className="mx-3">/</span> Contact</p>
@@ -27,46 +27,47 @@ function Contact() {
             {
               getProps => {
                 return <>
-                  <div className="flex-1 ml-auto flex space-x-8 w-full justify-between">
+                  <div className="flex-1 ml-auto md:flex md:space-x-8 space-y-6 w-full justify-between items-baseline">
                     <div  {...getProps(0, "flex items-start space-x-4 flex-1")} >
                       <div className="text-primary text-icon w-10 h-10 flex justify-center items-center flex-shrink-0"><Location /></div>
-                      <div className="text-xl leading-10">34 Street Name, City Name Here, United States</div>
+                      <div className="text-xl leading-10">{get('global.contact.address')}</div>
                     </div>
-                    <div  {...getProps(1, "flex items-start space-x-4 flex-1 justify-center")} >
-                      <div className="text-primary text-icon w-10 h-10 flex justify-center items-center flex-shrink-0">
-                        <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M497.39 361.8l-112-48a24 24 0 0 0-28 6.9l-49.6 60.6A370.66 370.66 0 0 1 130.6 204.11l60.6-49.6a23.94 23.94 0 0 0 6.9-28l-48-112A24.16 24.16 0 0 0 122.6.61l-104 24A24 24 0 0 0 0 48c0 256.5 207.9 464 464 464a24 24 0 0 0 23.4-18.6l24-104a24.29 24.29 0 0 0-14.01-27.6z"></path></svg>
+                    <div  {...getProps(1, "flex items-start space-x-4 flex-1 md:justify-center")} >
+                      <div className="text-primary text-icon w-10 h-10 flex md:justify-center md:items-center flex-shrink-0">
+                        <svg stroke="currentColor" fill="currentColor" strokeWidth={0} viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M497.39 361.8l-112-48a24 24 0 0 0-28 6.9l-49.6 60.6A370.66 370.66 0 0 1 130.6 204.11l60.6-49.6a23.94 23.94 0 0 0 6.9-28l-48-112A24.16 24.16 0 0 0 122.6.61l-104 24A24 24 0 0 0 0 48c0 256.5 207.9 464 464 464a24 24 0 0 0 23.4-18.6l24-104a24.29 24.29 0 0 0-14.01-27.6z" /></svg>
                       </div>
-                      <div className="text-xl leading-10">+1 242 4942 290</div>
+                      <div className="text-xl leading-10">{get('global.contact.phone')}</div>
                     </div>
-                    <div  {...getProps(2, "flex items-start space-x-4 flex-1 justify-end")} >
+                    <div  {...getProps(2, "flex items-start space-x-4 flex-1 md:justify-end")} >
                       <div className="text-primary text-icon w-10 h-10 flex justify-center items-center flex-shrink-0">
                         <svg stroke="currentColor" fill="currentColor" strokeWidth={0} viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" /></svg>
                       </div>
-                      <div className="text-xl leading-10">info@yourdomain.coms</div>
+                      <div className="text-xl leading-10">{get('global.contact.email')}</div>
                     </div>
                   </div>
-                  <from {...getProps(3, "mb-10 md:mb-0  grid grid-cols-2 gap-8 order-1 ")} >
-                    <fieldset className="grid-cols-1 flex flex-col space-y-2">
+                  <form method="POST" action="/api/contact" {...getProps(3, "mb-10 md:mb-0 space-y-8 items-baseline md:grid grid-cols-2 gap-x-8 order-1 ")} >
+                    <fieldset className="flex flex-col space-y-2">
                       <label>Name</label>
-                      <input type='text' className="py-3 px-5 border-2" />
+                      <input required name="Name" type='text' className="py-3 px-5 border-2 focus:border-primary" />
                     </fieldset>
                     <fieldset className="grid-cols-1 flex flex-col space-y-2">
                       <label>Phone</label>
-                      <input type='text' className="py-3 px-5 border-2" />
+                      <input name="Phone" type='text' className="py-3 px-5 border-2 focus:border-primary" />
                     </fieldset>
-                    <fieldset className="col-span-2 flex flex-col space-y-2">
+                    <fieldset className="flex flex-col space-y-2">
                       <label>Email</label>
-                      <input type='email' className="py-3 px-5 border-2" />
+                      <input required name="Email" type='email' className="py-3 px-5 border-2 focus:border-primary" />
                     </fieldset>
                     <fieldset className="col-span-2 flex flex-col space-y-2">
                       <label>Write Message</label>
-                      <textarea rows="5" type='text' className="py-3 px-5 border-2" />
+                      <textarea required name="message" rows="5" type='text' className="py-3 px-5 border-2 focus:border-primary" />
                     </fieldset>
                     <div>
-
+                      <button type="submit" className="border-2 border-black font-bold text-black truncate text-center uppercase px-5 py-3 hover:text-white hover:bg-black transition-colors">
+                        Send message
+                      </button>
                     </div>
-
-                  </from>
+                  </form>
 
                 </>
               }
---
+++ pages/api/contact.js
@@ -0,0 +1,73 @@
+
+
+const loginApi = async () => {
+  const identifier = process.env.STRAPI_IDENTIFIER
+  const password = process.env.STRAPI_PASSWORD
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      identifier,
+      password
+    }),
+  }
+  console.log('loginApi', { options })
+  const { jwt, user, ...rest } = await fetch(process.env.API_URL + '/auth/local', options).then(res => {
+    return res.json()
+  })
+  return { jwt, user, ...rest }
+}
+const sendContactApi = async (req, jwt) => {
+  const { query = {} } = req
+  let text = '';
+  for (const property in query) {
+
+    text += `${property}: ${query[property]} ` + "\n";
+  }
+  console.log(text)
+  const body = JSON.stringify({
+    to: process.env.STRAPI_EMAIL_TO || "hieunguyenel@gmail.com",
+    subject: 'contact us',
+    text
+  })
+  const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + jwt
+  }
+  const result = await fetch(process.env.API_URL + '/email', {
+    method: 'POST',
+    headers,
+    body,
+  }).then(res => {
+    return res.json()
+  })
+  console.log({ result })
+  const { error, message = 'system error' } = result
+  if (error) {
+    throw new Error(message)
+  }
+}
+
+export default async function contact(req, res) {
+  // Exit the current user from "Preview Mode". This function accepts no args.
+  // Redirect the user back to the index page.
+  res.writeHead(307, { Location: '/contact?posted=true', })
+  res.end()
+
+  try {
+    const { jwt, user, ...rest } = await loginApi()
+    console.log({ user, jwt, ...rest })
+    if (!jwt) {
+      throw new Error("login failed")
+    }
+    console.log('login success')
+    if (jwt) {
+      await sendContactApi(req, jwt)
+      console.log('sent contact successfull')
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
